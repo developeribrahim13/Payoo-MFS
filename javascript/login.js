e386b3ac8fb3115a77ad4372c85c778b30155b76
Md Ibrahim Khalil <developeribrahim13@gmail.com>
2026-02-18 16:38:46 +0600

    add favicon and pin  and number validation for login to homepage redirection

---
+++ javascript/login.js
@@ -0,0 +1,20 @@
+document.getElementById('login-btn')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        const mobileNumber = document.getElementById('mobile-number').value;
+        const pinNumber = document.getElementById('pin-number').value;
+        
+        if(mobileNumber=== '01846552436' && pinNumber=== '1234')
+        {
+            alert("welcome to the Home Page");
+            // window.location.href="/homepage.html"; eivabew lekha jay
+            window.location.assign("/homepage.html");
+        }
+        else
+        {
+            alert("login failed! Pls try again.");
+        }
+
+        document.getElementById('mobile-number').value = "";
+        document.getElementById('pin-number').value = "";
+    });
